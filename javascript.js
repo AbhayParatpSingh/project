50a8702848827fffc7bf5c0ece6492b4d9f52947
AbhayPratapSingh <abhaypratapsingh16072004@gmail.com>
2023-01-06 23:43:02 +0530

    day 3 end

---
+++ javascript.js
@@ -0,0 +1,15 @@
+console.log('welcome to spotify clone');
+let songs =[
+    { 
+        song:"Um Poco Loco", filepath:"A:\New folder (2)\um poco loco.mp3",
+        song:"Illzam", filepath:"A:\Music\Ilzaam - Arjun Kanungo.mp3",
+        song:"Ankho Se Batana", filepath:"A:\New folder (2)\Aankhon Se Batana(PagalWorld.com.se).mp3",
+        song:"radha rani", filepath:"A:\New folder (2)\Radha-Rani(PagalWorld).mp3",
+        song:"Shri Raghunath", filepath:"A:\New folder (2)\Hamare-Sath-Shri-Raghunath(PagalWorldl).mp3",
+        song:"8 parche", filepath:"A:\New folder (2)\8-Parche(PagalWorldl).mp3"
+
+
+    }
+]
+let audioElement = new Audio('um poco loco.mp3');
+//audioElement.play();
