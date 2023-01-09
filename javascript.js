60deab7a855070ce6b3f032ac4aac1681dc9d243
AbhayPratapSingh <abhaypratapsingh16072004@gmail.com>
2023-01-09 11:10:25 +0530

    day 5 start

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 console.log('welcome to spotify clone');
 
 let songIndex=0;
-let audioElement=new Audio('songs/um poco loco.mp3');
+let audioElement=new Audio('songs/1.mp3');
 let masterplay= document.getElementById('masterplay');
 let progressBar= document.getElementById('progressBar');
 let gif= document.getElementById('gif');
@@ -11,13 +11,13 @@ let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs =[
     
-       { songName:"Um Poco Loco",filePath:"songs/um poco loco.mp3"},
-        {songName:"Illzam",filePath:"songs/Ilzaam - Arjun Kanungo.mp3"},
-        {songName:"Ankho Se Batana",filePath:"songs/Aankhon Se Batana(PagalWorld.com.se).mp3"},
-        {songName:"radha rani",filePath:"songs/Radha-Rani(PagalWorld).mp3"},
-        {songName:"Shri Raghunath",filePath:"songs/Hamare-Sath-Shri-Raghunath(PagalWorldl).mp3"},
-        {songName:"8 parche",filePath:"songs/8-Parche(PagalWorldl).mp3"},
-        {songName:"Teri ankho ka kajal",filePath:"songs/new_192_Teri Aakhya Ka Yo Kajal - D C Madana.mp3"},
+       { songName:"Um Poco Loco",filePath:"songs/1.mp3"},
+        {songName:"Illzam",filePath:"songs/2.mp3"},
+        {songName:"Ankho Se Batana",filePath:"songs/3.mp3"},
+        {songName:"radha rani",filePath:"songs/4.mp3"},
+        {songName:"Shri Raghunath",filePath:"songs/5.mp3"},
+        {songName:"8 parche",filePath:"songs/6.mp3"},
+        {songName:"Teri ankho ka kajal",filePath:"songs/7.mp3"},
 
 
     
---
+++ javascript2.js
@@ -0,0 +1,119 @@
+console.log('welcome to spotify clone');
+
+let songIndex=0;
+let audioElement=new Audio('psongs/1.mp3');
+let masterplay= document.getElementById('masterplay');
+let progressBar= document.getElementById('progressBar');
+let gif= document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+
+let songs =[
+    
+       { songName:"Ghungroo:Arijit Singh",filePath:"psongs/1.mp3"},
+        {songName:"Dil Diyan Gallan:Atif Aslam",filePath:"psongs/2.mp3"},
+        {songName:"Drau Desi:Benny Dayal",filePath:"psongs/3.mp3"},
+        {songName:"Pehli Nazar Me:Pritam",filePath:"psongs/4.mp3"},
+        {songName:"Khudaya Khair:Soham ",filePath:"psongs/5.mp3"},
+        {songName:"Lehra Do:Arijit Singh",filePath:"psongs/6.mp3"},
+        {songName:"Srivali:Javed Ali",filePath:"psongs/7.mp3"},
+
+
+    
+]
+songItems.forEach((element, i)=>{ 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+
+
+
+masterplay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterplay.classList.remove('fa-play-circle');
+        masterplay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
+    }
+    else{
+        audioElement.pause();
+       
+        masterplay.classList.remove('fa-pause-circle');
+        masterplay.classList.add('fa-play-circle');
+        gif.style.opacity=0;
+
+    }
+}
+)
+
+//listen to events
+audioElement.addEventListener('timeupdate',()=>{
+
+progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+progressBar.value =progress;
+
+})
+progressBar.addEventListener('change',()=>
+{
+    audioElement.currentTime = progressBar.value * audioElement.duration/100;
+})
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemplay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+   
+Array.from(document.getElementsByClassName('songItemplay')).forEach((element) => {
+    element.addEventListener('click',(e)=>{
+       
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `psongs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity=1;
+        masterplay.classList.remove('fa-play-circle');
+        masterplay.classList.add('fa-pause-circle');
+    
+        
+        
+
+    })
+    
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=6){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `psongs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterplay.classList.remove('fa-play-circle');
+    masterplay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `psongs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterplay.classList.remove('fa-play-circle');
+    masterplay.classList.add('fa-pause-circle');
+})
+
